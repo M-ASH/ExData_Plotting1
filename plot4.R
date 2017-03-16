2f492676e47a8322eda9a0e303319a144f2918a7
M-ASH <minnie.ashdown@gmail.com>
2017-03-16 17:05:07 +0000

    corrected plot 4

--- plot4.R
+++ plot4.R
@@ -36,17 +36,17 @@ Global_reactive_power <- as.numeric(as.character(data1$Global_reactive_power))
 
 par(mfrow=c(2,2))
 
+plot(datetime, GAP, type = "l", ylab = "Global Active Power (kW)", xlab =" ")
+plot(datetime, voltage, type ="l", xlab="datetime")
+
+plot(datetime, one, type = "l",col = "black", ylab = "Energy sub metering", xlab="")
+lines(datetime, two, type = "l", col = "red")
+lines(datetime, three, type = "l", col = "blue")
+
+legend('topright', c("sub metering 1", "sub metering 2", "sub metering 3"), lty=1, col=c('black', 'red', 'blue'), bty='n', cex=.75)
+
 plot(datetime, Global_reactive_power, type ="l")
 
-plot(datetime, voltage, type ="l", main = "Voltage", xlab = "Day")
-plot(datetime, GAP, type = "l", main ="Global Active Power", ylab = "Global Active Power (kW)", xlab = "Day")
-
-plot(datetime, one, type = "l",col = "black", ylab = "Energy sub metering", xlab = "Day", 
-     main = "Sub metering ")
-lines(datetime, two, type = "l", col = "blue")
-lines(datetime, three, type = "l", col = "red")
-
-legend('topright', c("sub metering 1", "sub metering 2", "sub metering 3"), lty=1, col=c('black', 'blue', 'red'), bty='n', cex=.75)
 
 dev.copy(png, file='plot4.png', height=480, width=480)
 dev.off()
